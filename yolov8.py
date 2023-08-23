b2844e576266edebf7f352eeecf5a010dbbbc539
Chia Chung <110074234+chiachung-tmlm@users.noreply.github.com>
2023-08-23 16:55:20 +0800

    Update yolov8.py

--- yolov8.py
+++ yolov8.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 
 if __name__ == '__main__':
-    model = YOLO('yolov8n.yaml')  # build a new model from scratch
+    model = YOLO('yolov8s.yaml')  # build a new model from scratch
 
-    results = model.train(data='config.yaml', epochs=10)  # train the model
+    results = model.train(data='config.yaml', epochs=50, workers=2)  # train the model
 
