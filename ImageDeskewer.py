b3db598bdd9aa2d099dae5320db9caa13b467c40
Chia Chung <110074234+chiachung-tmlm@users.noreply.github.com>
2023-08-30 09:46:29 +0800

    Create ImageDeskewer.py

---
+++ ImageDeskewer.py
@@ -0,0 +1,50 @@
+import os
+import numpy as np
+from skimage import io
+from skimage.color import rgb2gray
+from skimage.transform import rotate
+from deskew import determine_skew
+
+
+class Deskew:
+    def __init__(self, directory):
+        self.directory = directory
+
+    def processor(self):
+        # List all the image files in the folder
+        images = [f for f in os.listdir(self.directory) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+
+        # Iterate through each image file and deskew
+        for index, img in enumerate(images):
+            image_path = os.path.join(self.directory, img)
+            print(f'{str(index + 1)}. {image_path}')
+            self.deskewer(image_path)
+
+    """
+    Deskews an image and saves it back to the input path.
+    @:param input_path (str)
+    """
+    @staticmethod
+    def deskewer(input_path):
+        # Load the image
+        image = io.imread(input_path)
+
+        # Convert the image to grayscale
+        grayscale = rgb2gray(image)
+
+        # Determine the skew angle of the image
+        angle = determine_skew(grayscale)
+
+        # Rotate the image to correct the skew and scale it back to 8-bit
+        rotated = rotate(image, angle, resize=True) * 255
+        rotated = rotated.astype(np.uint8)
+
+        # Save the deskewed image back to the input path
+        io.imsave(input_path, rotated)
+
+
+if __name__ == "__main__":
+    # Input directory can be provided as a command-line argument or hardcoded here
+    input_directory = r'CVAT/Training Set'
+    deskewer = Deskew(input_directory)
+    deskewer.processor()
