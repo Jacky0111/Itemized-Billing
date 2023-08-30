b6efe6daa52dc9f0e8266625ddb31946d24836de
Chia Chung <110074234+chiachung-tmlm@users.noreply.github.com>
2023-08-30 11:45:39 +0800

    Update Folder.py

--- Folder.py
+++ Folder.py
@@ -2,10 +2,16 @@ import os
 
 
 class Folder:
+    """
+    Get the name of the most recently modified folder in a directory.
+    @:param directory (str): The path to the directory to search for folders.
+    @:return latest_folder (str or None): The name of the most recently modified folder within the specified directory,
+                                          or None if no folders are found.
+    """
     @classmethod
     def getLastModifiedFolder(cls, directory):
         # Initialize variables to track the last modified folder and its timestamp
-        last_modified_folder = None
+        latest_folder = None
         last_modified_timestamp = 0
 
         # Iterate through the directories in the specified directory
@@ -20,9 +26,22 @@ class Folder:
                 # Compare timestamps to find the most recent folder
                 if folder_timestamp > last_modified_timestamp:
                     last_modified_timestamp = folder_timestamp
-                    last_modified_folder = folder
+                    latest_folder = folder
 
-        return last_modified_folder
+        return latest_folder
+
+    '''
+    Create folders under a specified directory path.
+    @:param dir_path (str): The base directory path where folders will be created.
+    @:param parent_folder (str): The name of the parent folder under which sub-folders will be created.
+    @:param folder_names (list): A list of folder names to create as sub-folders of the parent folder.
+    '''
+    @staticmethod
+    def createFolders(dir_path, parent_folder, folder_names):
+        for folder_name in folder_names:
+            folder_path = os.path.join(dir_path, parent_folder, folder_name)
+            os.makedirs(folder_path, exist_ok=True)
+            print(f"Folder '{folder_name}' created at '{dir_path}'")
 
 
 if __name__ == "__main__":
