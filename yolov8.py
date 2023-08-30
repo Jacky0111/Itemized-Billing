ec91f325d093775ef38ef710dd02f74a3fe9b2a6
Chia Chung <110074234+chiachung-tmlm@users.noreply.github.com>
2023-08-30 11:46:50 +0800

    Delete yolov8.py

--- yolov8.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-
-if __name__ == '__main__':
-    model = YOLO('yolov8s.yaml')  # build a new model from scratch
-
-    results = model.train(data='config.yaml', epochs=1000, workers=2)  # train the model
-
