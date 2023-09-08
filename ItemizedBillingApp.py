3853947123c0fcd517bbc430300f77b9b578e433
Chia Chung <110074234+chiachung-tmlm@users.noreply.github.com>
2023-09-08 10:44:51 +0800

    Update ItemizedBillingApp.py

--- ItemizedBillingApp.py
+++ ItemizedBillingApp.py
@@ -2,28 +2,26 @@ import os
 import re
 import wx
 import shutil
-
 from pathlib import Path
 from datetime import datetime
 
 from Detect import Detect
-from PDF_To_Image.Conversion import PDFToImageConverter
+from Conversion.Conversion import Converter
 
 
 class ItemizedBillingApp:
-    data = []
-    images = []  # To store the image converted from chosen invoice
-    dataset_images = []  # To store the entire converted samples data
-    invoice = None  # Chosen invoice
-    img_counter = 0
-    file_name = None
-
+    xml_path = None
+    text_path = None
     images_path = None
     dataset_path = None
     output_folder_path = []
 
     def __init__(self):
-        pass
+        self.xml_path = None
+        self.text_path = None
+        self.images_path = None
+        self.dataset_path = None
+        self.output_folder_path.clear()
 
     '''
     Execution function
@@ -34,21 +32,37 @@ class ItemizedBillingApp:
         if choice == 1:
             print('--------------------------------------Generating Dataset--------------------------------------')
 
-            self.dataset_path = self.setFolderPath(subfolder='pdf', parent='PDF_To_Image')
-            self.images_path = self.setFolderPath(subfolder='images', parent='PDF_To_Image')
+            self.dataset_path = self.setFolderPath(subfolder='pdf', parent='Conversion')
+            self.images_path = self.setFolderPath(subfolder='images', parent='Conversion')
 
             print('----------------------------------------Choosing File-----------------------------------------')
             # Use the file selection dialog to choose a file(s)
             bill_path = self.chooseFile()
 
             # Remove existing files in pdf folder and copy select files
-            self.processSelectedFiles(bill_path)
+            self.processSelectedFiles(bill_path, self.dataset_path)
 
             print('-----------------------------------Converting PDF to image------------------------------------')
-            converter = PDFToImageConverter()
-            converter.convertMultiplePdfs(self.dataset_path, self.images_path)
+            converter = Converter()
+            converter.pdfToImages(self.dataset_path, self.images_path)
 
         elif choice == 2:
+            print('--------------------------------------Generating Dataset--------------------------------------')
+            self.text_path = self.setFolderPath(subfolder='text', parent='Conversion')
+            self.xml_path = self.setFolderPath(subfolder='xml', parent='Conversion')
+
+            print('----------------------------------------Choosing File-----------------------------------------')
+            # Use the file selection dialog to choose a file(s)
+            bill_path = self.chooseFile(allow_images=True)
+
+            # Remove existing files in pdf folder and copy select files
+            self.processSelectedFiles(bill_path, self.xml_path)
+
+            print('------------------------------------Converting TXT to XML-------------------------------------')
+            # converter = Converter()
+            # converter.pdfToImages(self.dataset_path, self.images_path)
+
+        elif choice == 3:
             print('----------------------------------------Choosing File-----------------------------------------')
             # Use the file selection dialog to choose a file(s)
             img_path = self.chooseFile()
@@ -73,9 +87,6 @@ class ItemizedBillingApp:
 
             # print('-----------------------------------------Applying OCR-----------------------------------------')
 
-
-
-
     '''
     A main menu that allows user to choose either create a dataset or run ocr.
     @:return int
@@ -85,7 +96,8 @@ class ItemizedBillingApp:
         print('MAIN MENU')
         print('=========')
         print('1. Create Dataset')
-        print('2. Run OCR')
+        print('2. Create XML file(s)')
+        print('3. Run OCR')
         return int(input('Enter your choice: '))
 
     '''
@@ -118,10 +130,15 @@ class ItemizedBillingApp:
     @return path
     '''
     @staticmethod
-    def chooseFile():
+    def chooseFile(allow_images=False):
         app = wx.App(None)
         style = wx.FD_OPEN | wx.FD_FILE_MUST_EXIST | wx.FD_MULTIPLE | wx.DD_DIR_MUST_EXIST
-        dialog = wx.FileDialog(None, 'Open', style=style)
+        wildcard = "All files (*.*)|*.*"
+
+        if allow_images:
+            wildcard = "Image files (*.png;*.jpg;*.jpeg;*.bmp)|*.png;*.jpg;*.jpeg;*.bmp|" + wildcard
+
+        dialog = wx.FileDialog(None, 'Open', wildcard=wildcard, style=style)
 
         if dialog.ShowModal() == wx.ID_OK:
             paths = dialog.GetPaths()
@@ -132,16 +149,17 @@ class ItemizedBillingApp:
 
         return paths
 
-    def processSelectedFiles(self, files):
+    @staticmethod
+    def processSelectedFiles(files, destination):
         # Remove existing files in the destination folder
-        for existing_file in os.listdir(self.dataset_path):
-            file_path = os.path.join(self.dataset_path, existing_file)
+        for existing_file in os.listdir(destination):
+            file_path = os.path.join(destination, existing_file)
             if os.path.isfile(file_path):
                 os.remove(file_path)
 
         # Copy the selected files to the destination folder
         for path in files:
-            shutil.copy(path, self.dataset_path)
+            shutil.copy(path, destination)
 
 
 if __name__ == "__main__":
