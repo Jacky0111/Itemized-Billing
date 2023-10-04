f71a67326629ab6989a0325d99adc54b7372d54b
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2023-10-05 04:20:30 +0800

    Update Detect.py

--- Detect.py
+++ Detect.py
@@ -97,7 +97,7 @@ class Detect:
         print(f'Saved path: {saved_path}')
         print(f'Image name: {image_name}')
 
-        parser.add_argument('--weights', nargs='+', type=str, default='best.pt', help='model.pt path(s)')
+        parser.add_argument('--weights', nargs='+', type=str, default='table.pt', help='model.pt path(s)')
         parser.add_argument('--source', type=str, default=saved_path, help='source')  # file/folder, 0 for webcam
         parser.add_argument('--img-size', type=int, default=416, help='inference size (pixels)')
         parser.add_argument('--conf-thres', type=float, default=0.8, help='object confidence threshold')
