84c4c2a0afc6d019b3e2b921f7e5697cdda19b24
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2023-10-06 09:40:31 +0800

    Update RenameImages.py

--- RenameImages.py
+++ RenameImages.py
@@ -1,18 +1,54 @@
 import os
 
-folder_path = r'C:\Users\ChiaChungLim\PycharmProjects\Itemized-Billing\CVAT\KPJ'
-new_prefix = 'KPJ_'
 
-# Iterate over the files in the folder
-for i, filename in enumerate(os.listdir(folder_path)):
-    # Check if the file is a .png file (you can change the extension if needed)
-    if filename.endswith('.png'):
-        # Generate the new filename
-        new_filename = f'{new_prefix}{i + 101}.png'
+class Rename:
+    def __init__(self, folder_path, new_prefix='KPJ_'):
+        self.folder_path = folder_path
+        self.new_prefix = new_prefix
 
-        # Create the full paths for the old and new filenames
-        old_path = os.path.join(folder_path, filename)
-        new_path = os.path.join(folder_path, new_filename)
+    def scenario1(self):
+        # Iterate over the files in the folder
+        for i, filename in enumerate(os.listdir(self.folder_path)):
+            # Check if the file is a .png file (you can change the extension if needed)
+            if filename.endswith('.png'):
+                # Generate the new filename
+                new_filename = f'{self.new_prefix}{i + 101}.png'
 
-        # Rename the file
-        os.rename(old_path, new_path)
+                # Create the full paths for the old and new filenames
+                old_path = os.path.join(self.folder_path, filename)
+                new_path = os.path.join(self.folder_path, new_filename)
+
+                # Rename the file
+                os.rename(old_path, new_path)
+
+    def scenario2(self):
+        allowed_extensions = ['.png', '.jpg', '.jpeg']  # Add more extensions as needed
+
+        # Iterate over the files in the folder
+        for filename in os.listdir(self.folder_path):
+            # Check if the file has an allowed image extension
+            if any(filename.endswith(ext) for ext in allowed_extensions) and 'crop' in filename:
+                # Remove the last 4 characters from the filename (excluding the extension)
+                base_filename, file_extension = os.path.splitext(filename)
+                new_filename = f'{base_filename[:-5]}{file_extension}'
+
+                # Create the full paths for the old and new filenames
+                old_path = os.path.join(self.folder_path, filename)
+                new_path = os.path.join(self.folder_path, new_filename)
+
+                # Rename the file
+                os.rename(old_path, new_path)
+
+
+if __name__ == "__main__":
+    # folder_path = r'C:\Users\ChiaChungLim\PycharmProjects\Itemized-Billing\CVAT\KPJ'
+    folder_path = r'C:\Users\CP1\Documents\GitHub\Itemized-Billing\Crop_Images'
+    new_prefix = 'KPJ_'
+
+    renamer = Rename(folder_path, new_prefix)
+
+    # To execute scenario1:
+    # renamer.scenario1()
+
+    # To execute scenario2:
+    renamer.scenario2()
