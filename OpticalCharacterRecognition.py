6b95fc2cad273cb9d3e1f5a6755376ded706827a
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2023-10-24 15:26:29 +0800

    Update OpticalCharacterRecognition.py

--- OpticalCharacterRecognition.py
+++ OpticalCharacterRecognition.py
@@ -1,2 +1,45 @@
+import json
+import os
+import re
+import io
+
+import numpy as np
+import pandas as pd
+
+from cv2 import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
 class OCR:
-    pass
+    path = None  # Current save path
+    image = None  # Input image
+    header = None
+    content = None
+    is_non_native = False
+
+    counter = 0
+
+    data = dict()
+
+    def __init__(self, path):
+        self.data.clear()
+        self.path = path
+
+    '''
+    Execution function
+    @param img_path
+    '''
+    def runner(self, img_path):
+        img = cv2.imread(img_path)
+        data, df = self.imageToData(img, r'--oem 3 --psm 4 -l eng')
+        df = df.loc[:, 'left':]
+        self.drawBoundingBox(img, data)
+
+        print(f'Current counter is {self.counter}')
+        self.data, self.counter = self.header.runner(df, self.counter) if self.identifyClass(
+            os.path.split(img_path)[1]) == 'head' \
+            else self.content.runner(df, img, self.counter, self.path)
+        print(f'Current counter is {self.counter}')
+        self.saveToJson(self.path)
