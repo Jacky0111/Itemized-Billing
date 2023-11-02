2979da12e0085031818f6b9c55808bb32e0aedd7
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2023-11-03 03:08:01 +0800

    Create Body.py

---
+++ Body.py
@@ -0,0 +1,12 @@
+from Bill import Bill
+
+
+class Body(Bill):
+    TYPE_BODY = 'body'  # Type of content
+
+    def __init__(self, x=0, y=0, width=0, height=0, conf=0, text=None, identity=None):
+        super().__init__(x, y, width, height, conf, text, identity)
+        self.identity = self.TYPE_BODY
+
+    def __str__(self):
+        return super.__str__(self)
