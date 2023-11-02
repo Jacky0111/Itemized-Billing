43f5a9120824feb96b2d76c737f8b67cc5cb0c94
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2023-11-03 03:08:08 +0800

    Update OpticalCharacterRecognition.py

--- OpticalCharacterRecognition.py
+++ OpticalCharacterRecognition.py
@@ -5,10 +5,10 @@ import cv2
 import numpy as np
 import pandas as pd
 
+from TabularRule import TabularRule
+
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-from TabularRule import TabularRule
 
 
 class OCR:
@@ -19,12 +19,11 @@ class OCR:
     is_non_native = False
 
     counter = 0
-    data = dict()
+    df = pd.DataFrame()
     table_data_list = []
     data_coordinate_list = []
 
     def __init__(self, output_path, images_path):
-        self.data.clear()
         self.output_path = output_path
         self.images_path = images_path
 
@@ -33,10 +32,8 @@ class OCR:
     '--oem 3' uses default LSTM OCR engine mode.
     '--psm 4' represents the Page Segmentation Mode and 4 assumes a single column of text.
     '''
-    def runner(self):
-        df = pd.DataFrame()
-        content = ''
 
+    def runner(self):
         # Loop through all images
         for idx, file in enumerate(os.listdir(self.images_path)):
             # Construct the full path of the current image file
@@ -48,33 +45,27 @@ class OCR:
             temp_df = temp_df.loc[:, 'left':]
 
             # Concatenate the data to the final DataFrame
-            df = pd.concat([df, temp_df], ignore_index=True)
+            self.df = pd.concat([self.df, temp_df], ignore_index=True)
 
             # Draw bounding boxes on the image
             self.drawBoundingBox(img, data)
-
             cv2.imwrite(self.images_path + f'/bbox_{file}', img)
 
             # Store the bill in tabular format
-            tr = TabularRule(temp_df)
-            row = tr.runner()
-
-            print(f'row_list: {row}')
-            print('=========================================================')
-
-            self.table_data_list.append(row)
+            tr = TabularRule(temp_df, True if idx == 0 else False)
+            tr.runner()
+            self.table_data_list.append(tr.row_list)
 
         for i, ele in enumerate(self.table_data_list):
             print(f'{i}. {ele}')
 
-        # df_new = pd.DataFrame(self.table_data_list[1:], columns=self.table_data_list[0])
-        # df_new.to_csv(f'{self.output_path}/output.csv', index=False)
-        self.saveToCSV(df)
+        self.saveToCSV(self.df)
 
     '''
     Saved recognized text to json file
     @param path
     '''
+
     def saveToCSV(self, data):
         data.to_csv(f'{self.output_path}/image_to_data.csv', index=False)
 
@@ -84,6 +75,7 @@ class OCR:
     @param config
     @return data, df
     '''
+
     @staticmethod
     def imageToData(img, config):
         data = pytesseract.image_to_data(img, config=config)
@@ -98,6 +90,7 @@ class OCR:
     @param img
     @param boxes
     '''
+
     @staticmethod
     def drawBoundingBox(img, boxes):
         red = (0, 0, 255)
@@ -118,6 +111,7 @@ class OCR:
     Check whether the confidence scores of the image is high enough in order to determine native and non-native pdf
     @param df
     '''
+
     @staticmethod
     def checkConfidenceScore(df):
         df_conf = df.loc[(df['conf'] > 0) & (df['conf'] <= 70)]
@@ -134,6 +128,7 @@ class OCR:
     @param name
     @return cls
     '''
+
     @staticmethod
     def identifyClass(name):
         cls = None
