73150098c3db22bdda4213b2b5c53a36f7fd5403
Chia Chung <69098532+Jacky0111@users.noreply.github.com>
2024-03-15 15:00:54 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+import os
+# Define the path
+path = r"C:\Users\ChiaChungLim\PycharmProjects\Consignment-Itemized-Data\data\KPJ_01"
+# Change directory
+os.chdir(path)
+# List all files in the directory
+files = os.listdir()
+# Filter out directories (if any)
+files = [f for f in files if os.path.isfile(f)]
+# Sort files based on their names
+files.sort()
+# Define a counter for renaming
+counter = 1
+# Rename files
+for file_name in files:
+    # Split the file name and extension
+    name, ext = os.path.splitext(file_name)
+    # Generate new file name with format KPJ_001, KPJ_002, ...
+    new_name = f"KPJ_{counter:03}{ext}"
+    # Rename the file
+    os.rename(file_name, new_name)
+    # Increment counter
+    counter += 1
+print("Files renamed successfully.")
